4a21aefd5a5f6676d2b01e4c8fde865ab4d9a6ca
Myitian <miaoyitian233@gmail.com>
2025-10-17 20:20:28 +0800

    support 1.19.3~1.20.4

--- common/src/main/java/net/myitian/no_caves/mixin/RegistryLoaderMixin.java
+++ common/src/main/java/net/myitian/no_caves/mixin/RegistryLoaderMixin.java
@@ -1,34 +1,34 @@
 package net.myitian.no_caves.mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
-import com.mojang.serialization.DataResult;
-import com.mojang.serialization.Decoder;
-import net.minecraft.registry.MutableRegistry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryLoader;
-import net.minecraft.registry.RegistryOps;
-import net.minecraft.resource.ResourceManager;
 import net.myitian.no_caves.RegistryLoaderHelper;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.util.Map;
-import java.util.function.Consumer;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(RegistryLoader.class)
 abstract class RegistryLoaderMixin {
-    @Redirect(
+    @Unique
+    private static final ThreadLocal<RegistryKey<?>> tmp$registryKey = new ThreadLocal<>();
+
+    @ModifyVariable(
             method = "load(Lnet/minecraft/registry/RegistryOps$RegistryInfoGetter;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/registry/RegistryKey;Lnet/minecraft/registry/MutableRegistry;Lcom/mojang/serialization/Decoder;Ljava/util/Map;)V",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lcom/mojang/serialization/DataResult;getOrThrow(ZLjava/util/function/Consumer;)Ljava/lang/Object;",
-                    remap = false))
-    private static Object parseAndAdd_Redirect_getOrThrow(
-            DataResult<?> instance,
-            boolean allowPartial,
-            Consumer<String> onError,
-            @Local(ordinal = 1) RegistryKey<?> registryKey) {
-        return RegistryLoaderHelper.process(registryKey, instance.getOrThrow(allowPartial, onError));
+            at = @At("STORE"),
+            ordinal = 1)
+    private static RegistryKey<?> load_ModifyVariable_of(RegistryKey<?> key) {
+        tmp$registryKey.set(key);
+        return key;
+    }
+
+    @ModifyVariable(
+            method = "load(Lnet/minecraft/registry/RegistryOps$RegistryInfoGetter;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/registry/RegistryKey;Lnet/minecraft/registry/MutableRegistry;Lcom/mojang/serialization/Decoder;Ljava/util/Map;)V",
+            at = @At("STORE"),
+            ordinal = 0)
+    private static Object load_ModifyVariable_getOrThrow(Object value) {
+        RegistryKey<?> key = tmp$registryKey.get();
+        tmp$registryKey.remove();
+        return RegistryLoaderHelper.process(key, value);
     }
 }
